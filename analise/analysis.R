d8ae3d1fec96802c2cb14bf1eec4dfd54030f02a
Marcos Ronaldo Pereira Junior <marcos.rpj2@gmail.com>
2015-07-02 00:46:44 -0300

    editado script R para gerar gráfico de distribuição. Texto de validação de intervalos reestruturado

--- analise/analysis.R
+++ analise/analysis.R
@@ -1,26 +1,42 @@
-library(ggplot2)
-library(corrplot)
-library(MASS)
+# library(ggplot2)
+# library(MASS)
 
-setwd("~/tcc-latex/analise/global_percentis")
+setwd("~/tcc-latex/analise/data/fixed/")
 
-data <- read.csv("lcom4.csv", header=TRUE, sep=',')
+data <- read.csv("android-5.1.0_r1.csv", header=TRUE, sep=',')
 
 # for(i in 3:39){
 
-  nome <- paste("~/tcc-latex/analise/global_percentis/","lcom4", ".pdf", sep = "")
+  nome <- paste("~/tcc-latex/analise/results/","android510dist", ".pdf", sep = "")
   pdf(file = nome)
 
   classes <- data[,2]
   percentil75 <- data[,9]
 
-  # Boxplot to identify outliers  
-  # boxplot(metric, main=colnames(data)[i])
 
+# Kernel Density Plot
+  par(mfrow = c(2, 2)) 
+  dens_lcom4 <- density(data$lcom4) # returns the density data 
+  dens_acc <- density(data$acc) # returns the density data 
+  dens_accm <- density(data$accm) # returns the density data 
+  dens_rfc <- density(data$rfc) # returns the density data 
+
+
+
+
+  plot(dens_lcom4,main="LCOM4",xlab="",ylab="Densidade")
+  plot(dens_acc,main="ACC",xlab="",ylab="Densidade")
+  plot(dens_accm,main="ACCM",xlab="",ylab="Densidade")
+  plot(dens_rfc,main="RFC",xlab="",ylab="Densidade")
+
+
+  # Boxplot to identify outliers ,mainACCsidade LCOM4") # plots the results 
+  # boxplot(metric, ma,main="Densidade ACCM"),main="Densidade RFC")
   # nom <- data[,26]
   # amloc <- data[,5]
   # loc <- data[,21]
-  plot(classes,percentil75)#,xlim=c(0,35),ylim=c(0,35))  
+  #### plot(classes,percentil75)#,xlim=c(0,35),ylim=c(0,35))  
+
 
 # library(Hmisc)
 # t = 1:length(metric)
