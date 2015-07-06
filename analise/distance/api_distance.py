4986b8664a8bdffba5905b4bac39baa54cf4c190
Marcos Ronaldo Pereira Junior <marcos.rpj2@gmail.com>
2015-07-06 01:51:29 -0300

    criado texto inicial de comparação com API

--- analise/distance/api_distance.py
+++ analise/distance/api_distance.py
@@ -3,11 +3,13 @@ import sys
 import os
 import csv
 import numpy
+import operator
+
 HEADER='metric,min,1%,5%,10%,25%,50%,75%,90%,95%,99%,max'
 SYSTEM_UNIFIED_PERCENTILS= "/home/marcos/tcc-latex/analise/data/fixed/unified_percentis/"
 SYSTEM_APPS_PERCENTILS= "/home/marcos/tcc-latex/analise/apps_data/fixed/unified_percentis/"
-METRICS_LIST=["acc","accm","amloc","dit","lcom4","noc","rfc"]
-#METRICS_LIST=["acc","accm","amloc","lcom4","rfc"]
+# METRICS_LIST=["acc","accm","amloc","dit","lcom4","noc","rfc"]
+METRICS_LIST=["acc","accm","amloc","lcom4","dit","rfc"]
 SYSTEM_VALUES={"acc":{"75":3.0,"90":12.0,"95":26.0}, 
 				"accm":{"75":2.0,"90":4.0,"95":6.0},
 				"amloc":{"75":14.0,"90":31.0,"95":55.0},
@@ -17,14 +19,22 @@ SYSTEM_VALUES={"acc":{"75":3.0,"90":12.0,"95":26.0},
 				"rfc":{"75":31.0,"90":85.0,"95":140.0}
 				}
 
+# METRICS_WEIGHT={"acc":2,
+# 		"accm":2,
+# 		"amloc":1,
+# 		"dit":1,
+# 		"lcom4":2,
+# 		"noc":1,
+# 		"rfc":2,
+# 		"total":11
+# 		}
 METRICS_WEIGHT={"acc":2,
 		"accm":2,
-		"amloc":1,
+		"amloc":2,
 		"dit":1,
 		"lcom4":2,
-		"noc":1,
 		"rfc":2,
-		"total":11
+		"total":10
 		}
 
 
@@ -47,16 +57,16 @@ def get_limits():
 
 	for metric_file in files:
 		# minimum=[]
-		percentil95=[]
+		maximum=[]
 
 		with open(metric_file) as csvfile:
 			reader = csv.DictReader(csvfile)
 			for row in reader:
 				# minimum.append(row['min'])
-				percentil95.append(row['max'])
+				maximum.append(row['75%'])
 		metric = os.path.basename(metric_file)[:-4]
-		limits[metric]=float(max(percentil95))
-		# limits[metric]={"min":min(minimum),"max":(float(max(percentil95)))}
+		limits[metric]=float(max(maximum))
+		# limits[metric]={"min":min(minimum),"max":(float(max(maximum)))}
 	return limits
 
 def get_system_apps_percentis():
@@ -79,15 +89,35 @@ def get_similarity(app_metrics):
 	distances={}
 	for app in app_metrics:
 		total_distance=0
+		print app
 		for metric in METRICS_LIST:
 			metric_distance=0
 			for percentil in PERCENTIS:
-				metric_distance+= (abs(float(app_metrics[app][metric][percentil])-SYSTEM_VALUES[metric][percentil])/limits[metric])*PERCENTIS_WEIGHTS[percentil]
-			metric_distance/=95.0
+				metric_distance+= (abs(float(app_metrics[app][metric][percentil])-SYSTEM_VALUES[metric][percentil])/limits[metric])*PERCENTIS_WEIGHTS[percentil]			
+			metric_distance/=95.0			
 			metric_distance*=METRICS_WEIGHT[metric]
+			print "distance in metric",metric, ":",metric_distance
 			total_distance+=metric_distance
-		distances[app]=(int(100000*(total_distance/float(METRICS_WEIGHT["total"]))))
+		distances[app]=(int(100*(total_distance/float(METRICS_WEIGHT["total"]))))
 	return distances
+
+def get_relative_similarity(app_metrics):
+	limits = get_limits()
+	distances={}
+	for app in app_metrics:
+		total_distance=0
+		print app
+		for metric in METRICS_LIST:
+			metric_distance=0
+			for percentil in PERCENTIS:
+				metric_distance+= ((float(app_metrics[app][metric][percentil])-SYSTEM_VALUES[metric][percentil])/limits[metric])*PERCENTIS_WEIGHTS[percentil]			
+			metric_distance/=95.0			
+			metric_distance*=METRICS_WEIGHT[metric]
+			print "distance in metric",metric, ":",metric_distance
+			total_distance+=metric_distance
+		distances[app]=(int(100*(total_distance/float(METRICS_WEIGHT["total"]))))
+	return distances
+
 
 def mergeTXTfiles(path):
 
@@ -120,10 +150,10 @@ if __name__ == "__main__":
 			for row in reader:
 				metrics[appname][row['metric']]={"75":row['75%'],"90":row['90%'],"95":row['95%']}
 
-	result = get_similarity(get_system_apps_percentis())
+	result = get_relative_similarity(get_system_apps_percentis())
 	# print metrics
-	print get_similarity(metrics)
-
+	print get_relative_similarity(metrics)
+	print result	
 	l=[]
 	for app in result:
 		l.append(result[app])
@@ -133,7 +163,12 @@ if __name__ == "__main__":
 	print mean
 	print std
 
-	# removed={}
+	sorted_x = sorted(result.items(), key=operator.itemgetter(1))
+
+	with open("result.csv","w") as csvfile:
+		for values in sorted_x:
+			csvfile.write(values[0]+','+str(values[1])+'\n')
+
 	# for app in result:
 	# 	if (result[app] > (mean+std)) or (result[app] < (mean-std)):
 	# 		removed[app]=result[app]
--- analise/distance/verify.sh
+++ analise/distance/verify.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+TABLE_OUTDIR=~/tcc-latex/tables
+TABLE_FILE=relative_distance.tex
 
 Rscript loop_percentis.R /home/marcos/biodyn/biodynapp/biodyn-details.csv
-python normalize.py /home/marcos/biodyn/biodynapp/biodyn-details.csv
+python api_distance.py /home/marcos/biodyn/biodynapp/biodyn-details.csv
+
+tex_table=$TABLE_OUTDIR/$TABLE_FILE
+
+csv2latex result.csv > $tex_table
+
+head -n -1 $tex_table > temp.txt
+mv temp.txt $tex_table
+
+tail -n +5 $tex_table > temp.txt
+mv temp.txt $tex_table
